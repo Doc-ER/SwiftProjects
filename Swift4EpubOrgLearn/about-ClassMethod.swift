eb2a48e2b6b7267951869435298a179e2a09a5f7
Doc-ER <waynejszhang@outlook.com>
2017-06-14 09:51:51 +0800

    2017-06-13-2301

--- Swift4EpubOrgLearn/about-ClassMethod.swift
+++ Swift4EpubOrgLearn/about-ClassMethod.swift
@@ -1,1 +1,25 @@
 //Methods
+//Instance Methods
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+    }
+
+    func increment(by amount: Int) {
+        count += amount 
+    }
+
+    func reset(){
+        count = 0
+    }
+}
+
+let counter = Counter()
+
+counter.increment()
+
+counter.increment(by: 5)
+
+counter.reset()
+
