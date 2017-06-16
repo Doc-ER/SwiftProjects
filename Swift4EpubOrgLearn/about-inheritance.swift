a869cc66207d9eda6eb20600b40cd04942b07371
Doc-ER <waynejszhang@outlook.com>
2017-06-16 23:55:07 +0800

    2017-06-16-2354

--- Swift4EpubOrgLearn/about-inheritance.swift
+++ Swift4EpubOrgLearn/about-inheritance.swift
@@ -30,3 +30,29 @@ tandem.hasBasket = true
 tandem.currentNumberOfPassengers = 2
 tandem.currentSpeed = 22.0
 print("Tandem: \(tandem.description)")
+
+//Overriding
+class Train: Vehicle {
+    override func makeNoise(){
+        print("Choo Choo")
+    }
+}
+let train = Train()
+train.makeNoise()
+
+//Overriding Properties
+class Car: Vehicle {
+    var gear = 1
+    overide var description: String {
+        return super.description + "in gear \(gear)"
+    }
+}
+let car = Car()
+car.currentSpeed = 25.0
+ca.gear = 3
+print("Car: \(car.description)")
+
+//Preventing Overrides
+/*You can prevent a method, property, or subscript from being overridden by marking it as 
+final. Do this by writing the final modifier before the method, property, or subscript’s 
+introducer keyword (such as final var, final func, final class func, and final subscript).*/
